2260296df270297c715cbeed6851b36c5acaa30c
Juravlev Dmitrii <juravlevdima96@gmail.com>
2021-06-11 19:16:08 +0600

    add setup proxy

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Main = () => {
+  const [test, setTest] = useState('no')
+  useEffect(() => {
+    axios.get('/api/v1/test').then(({data}) => setTest(JSON.stringify(data)))
+  })
+  
   return (
     <div className="App">
       This is main page
+      <p>{test}</p>
     </div>
   )
 }
---
+++ client/src/setupProxy.js
@@ -0,0 +1,13 @@
+const createProxyMiddleware = require('http-proxy-middleware')
+
+const port = process.env.PORT || 8080
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: `http://localhost:${port}`,
+      changeOrigin: true,
+    })
+  )
+}
