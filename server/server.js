efa1bb268910a430afff346717a2e5105af5bd0e
Juravlev Dmitrii <juravlevdima96@gmail.com>
2021-06-29 16:18:20 +0600

    fix

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ server.get('/api/v1/test', (req, res) => {
   res.json({ status: 'OK' })
 })
 
-if (process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
   server.use(express.static(path.resolve('client/build')))
   server.get('/*', (req, res) => {
     res.sendFile(path.resolve('client/build/index.html'))
